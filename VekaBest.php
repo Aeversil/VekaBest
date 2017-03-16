e76a0399374353b7ce98ddf867da6de6f37c9b90
Steffan <Steffan>
2017-03-16 16:14:47 +0100

    VekaBest

--- VekaBest.php
+++ VekaBest.php
@@ -174,6 +174,7 @@
         </div>
         <div id="WinkelWagen" class="pagina">
           <p>Hier komt te staan wat mensen hebben bestelt </p>
+
         </div>
         <?php
           $conn->close();
