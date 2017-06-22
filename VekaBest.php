8ed7f5bdd2b61da9998960c51095560539602d4e
LAPTOP-SANDER\sande <sander.nieuwenhuisen@outlook.com>
2017-06-22 10:49:37 +0200

    Some minor changes.

--- VekaBest.php
+++ VekaBest.php
@@ -2,27 +2,27 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>VekaBest</title>
-  <script src="js/Teste.js"></script>
-  <link href="VekaBest.css" rel="stylesheet">
-  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-</head>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VekaBest</title>
+    <script src="js/Teste.js"></script>
+    <link href="VekaBest.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  </head>
   <body>
     <div class="Fullpage">
-      <!-- <div class="linkerbanner"><img src="stockvekafotos/stockbanner.jpg"></img></div> -->
-        <div class="banner"><img src="stockvekafotos/busbanner.jpg"></img></div>
-        <!-- NAVIGATIE BALK -->
-        <div class="navigation">
-          <button class="HomeButton"onclick="openPage('MainPage')">Home</buttons>
+        <!-- <div class="linkerbanner"><img src="stockvekafotos/stockbanner.jpg"></img></div> -->
+      <div class="banner"><img src="stockvekafotos/busbanner.jpg"></img></div>
+      <!-- NAVIGATIE BALK -->
+      <div class="navigation">
+        <button class="HomeButton"onclick="openPage('MainPage')">Home</buttons>
           <button onclick="openPage('Contact')">Contact</button>
           <!-- DROPDOWN MENU MET EEN SCRIPT -->
           <div class="dropdown">
-              <button onclick="dropdownmenu()" class="dropbtn">Webshop</button>
-              <div id="mydropdown" class="dropdown-content">
+            <button onclick="dropdownmenu()" class="dropbtn">Webshop</button>
+            <div id="mydropdown" class="dropdown-content">
               <!-- auto -->
               <a href="#" onclick="openPage('WebShopAuto')">Auto</a>
               <!-- brommer -->
@@ -38,144 +38,143 @@
             </div>
           </div>
           <button onclick="openPage('WinkelWagen')">WinkelWagen</button>
-        </div>
+      </div>
 
-        <!-- DEZE PAGINA'S WORDT MET JAVASCRIPT UITGEVOERT KIJK IN Teste.js OVER HOE EN WAT -->
-        <div id="MainPage" class="pagina">
-          <p>VUL HIER TEKST IN</p>
-          <p></p>
-        </div>
-        <div id="Contact" class="pagina">
-          <p>CONTACT INFORMATIE</p>
-        </div>
-        <div id="WebShopAuto" class="pagina">
-          <?php
-
-            $conn = new mysqli($host, $username, $password, $db_name);
-
-            if($conn->connect_error){
-              die("Connection failed:". $conn->connect_error);
-            }
-            $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'auto' ORDER BY boeksoort";
-            $result = $conn->query($sql);
-            if($result->num_rows > 0){
-              while ($row = $result->fetch_assoc()){
-                ?>
-                <div class='artikel'>
-                <?php
-                echo "<img src=".$row["boekafbeelding"]."></img><span>Productnummer: ".$row["boeksku"]. "</span><span>Boek: " .$row["boeknaam"]. "</span><span>Prijs: €" . $row["boekprijs"]."</span>";
-                ?>
-                <form action="VekaBest.php" method="$_POST">
-                  Aantal:<input type="text" name="aantal">
-                  <input type="image" src="stockvekafotos/winkelmand.png" width="25px" height="25px" border="0" alt="Submit" value="aantal"/>
-                </form>
-                </div>
-                <?php
-              }
-            }else{
-              echo "0 resultaten";
-            }
-            ?>
-        </div>
-        <div id="WebShopBrommer" class="pagina">
+      <!-- DEZE PAGINA'S WORDT MET JAVASCRIPT UITGEVOERT KIJK IN Teste.js OVER HOE EN WAT -->
+      <div id="MainPage" class="pagina">
+        <p>VUL HIER TEKST IN</p>
+        <p></p>
+      </div>
+      <div id="Contact" class="pagina">
+        <p>CONTACT INFORMATIE</p>
+      </div>
+      <div id="WebShopAuto" class="pagina">
           <?php
           $conn = new mysqli($host, $username, $password, $db_name);
 
-          if($conn->connect_error){
-            die("Connection failed:". $conn->connect_error);
+          if ($conn->connect_error) {
+            die("Connection failed:" . $conn->connect_error);
+          }
+          $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'auto' ORDER BY boeksoort";
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              ?>
+            <div class='artikel'>
+                <?php
+                echo "<img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span>";
+                ?>
+              <form action="VekaBest.php" method="$_POST">
+                Aantal:<input type="text" name="aantal">
+                <input type="image" src="stockvekafotos/winkelmand.png" width="25px" height="25px" border="0" alt="Submit" value="aantal"/>
+              </form>
+            </div>
+            <?php
+          }
+        } else {
+          echo "0 resultaten";
+        }
+        ?>
+      </div>
+      <div id="WebShopBrommer" class="pagina">
+          <?php
+          $conn = new mysqli($host, $username, $password, $db_name);
+
+          if ($conn->connect_error) {
+            die("Connection failed:" . $conn->connect_error);
           }
           $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'brommer' ORDER BY boeksoort";
           $result = $conn->query($sql);
-          if($result->num_rows > 0){
-            while ($row = $result->fetch_assoc()){
-                echo "<div class='artikel'><img src=".$row["boekafbeelding"]."></img><span>Productnummer: ".$row["boeksku"]. "</span><span>Boek: " .$row["boeknaam"]. "</span><span>Prijs: €" . $row["boekprijs"]."</span></div>";
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              echo "<div class='artikel'><img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span></div>";
             }
-          }else{
+          } else {
             echo "Er zijn op dit moment geen artikelen";
           }
           ?>
-        </div>
-        <div id="WebShopMotor" class="pagina">
+      </div>
+      <div id="WebShopMotor" class="pagina">
           <?php
           $conn = new mysqli($host, $username, $password, $db_name);
 
-          if($conn->connect_error){
-            die("Connection failed:". $conn->connect_error);
+          if ($conn->connect_error) {
+            die("Connection failed:" . $conn->connect_error);
           }
           $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'motor' ORDER BY boeksoort";
           $result = $conn->query($sql);
-          if($result->num_rows > 0){
-            while ($row = $result->fetch_assoc()){
-              echo "<div class='artikel'><img src=".$row["boekafbeelding"]."></img><span>Productnummer: ".$row["boeksku"]. "</span><span>Boek: " .$row["boeknaam"]. "</span><span>Prijs: €" . $row["boekprijs"]."</span></div>";
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              echo "<div class='artikel'><img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span></div>";
             }
-          }else{
+          } else {
             echo "0 resultaten";
           }
           ?>
-        </div>
-        <div id="WebShopVrachtwagen" class="pagina">
+      </div>
+      <div id="WebShopVrachtwagen" class="pagina">
           <?php
           $conn = new mysqli($host, $username, $password, $db_name);
 
-          if($conn->connect_error){
-            die("Connection failed:". $conn->connect_error);
+          if ($conn->connect_error) {
+            die("Connection failed:" . $conn->connect_error);
           }
           $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'vrachtwagen' ORDER BY boeksoort";
           $result = $conn->query($sql);
-          if($result->num_rows > 0){
-            while ($row = $result->fetch_assoc()){
-              echo "<div class='artikel'><img src=".$row["boekafbeelding"]."></img><span>Productnummer: ".$row["boeksku"]. "</span><span>Boek: " .$row["boeknaam"]. "</span><span>Prijs: €" . $row["boekprijs"]."</span></div>";
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              echo "<div class='artikel'><img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span></div>";
             }
-          }else{
+          } else {
             echo "0 resultaten";
           }
           ?>
-        </div>
-        <div id="WebShopBus" class="pagina">
+      </div>
+      <div id="WebShopBus" class="pagina">
           <?php
           $conn = new mysqli($host, $username, $password, $db_name);
 
-          if($conn->connect_error){
-            die("Connection failed:". $conn->connect_error);
+          if ($conn->connect_error) {
+            die("Connection failed:" . $conn->connect_error);
           }
           $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'bus' ORDER BY boeksoort";
           $result = $conn->query($sql);
-          if($result->num_rows > 0){
-            while ($row = $result->fetch_assoc()){
-              echo "<div class='artikel'><img src=".$row["boekafbeelding"]."></img><span>Productnummer: ".$row["boeksku"]. "</span><span>Boek: " .$row["boeknaam"]. "</span><span>Prijs: €" . $row["boekprijs"]."</span></div>";
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              echo "<div class='artikel'><img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span></div>";
             }
-          }else{
+          } else {
             echo "0 resultaten";
           }
           ?>
-        </div>
-        <div id="Spiegels" class="pagina">
+      </div>
+      <div id="Spiegels" class="pagina">
           <?php
           $conn = new mysqli($host, $username, $password, $db_name);
 
-          if($conn->connect_error){
-            die("Connection failed:". $conn->connect_error);
+          if ($conn->connect_error) {
+            die("Connection failed:" . $conn->connect_error);
           }
           $sql = "SELECT boeksoort, boeksku, boeknaam, boekafbeelding, boekprijs FROM boeken WHERE boeksoort LIKE 'spiegels' ORDER BY boeksoort";
           $result = $conn->query($sql);
-          if($result->num_rows > 0){
-            while ($row = $result->fetch_assoc()){
-              echo "<div class='artikel'><img src=".$row["boekafbeelding"]."></img><span>Productnummer: ".$row["boeksku"]. "</span><span>Boek: " .$row["boeknaam"]. "</span><span>Prijs: €" . $row["boekprijs"]."</span></div>";
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+              echo "<div class='artikel'><img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span></div>";
             }
-          }else{
+          } else {
             echo "0 resultaten";
           }
           ?>
-        </div>
-        <div id="WinkelWagen" class="pagina">
-          <p>Hier komt te staan wat mensen hebben bestelt </p>
-          <p>Kijken of we met cookies aan het werk moeten gaan, zodat we bestellingen vast kunnen zetten en kunnen dumpen naardat bestel compleet is.</p>
+      </div>
+      <div id="WinkelWagen" class="pagina">
+        <p>Hier komt te staan wat mensen hebben bestelt </p>
+        <p>Kijken of we met cookies aan het werk moeten gaan, zodat we bestellingen vast kunnen zetten en kunnen dumpen naardat bestel compleet is.</p>
 
-        </div>
-        <?php
-          $conn->close();
-        ?>
-        <!-- <div class="rechterbanner"><img src="stockvekafotos/stockbanner.jpg"></img></div> -->
+      </div>
+      <?php
+      $conn->close();
+      ?>
+      <!-- <div class="rechterbanner"><img src="stockvekafotos/stockbanner.jpg"></img></div> -->
     </div>
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <?php
-  $host="localhost";
-  $username="root";
-  $password="";
-  $db_name="vekabestwebsite";
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "vekabestwebsite";
 ?>
 
 <h1>Home page, add header and footer here</h1>
 <!-- User starts on this page -->
-  <!-- Clicks the Login button -->
-    <!-- if user go back to user home -->
-  <!-- Goes to shopping -->
-  <!-- Goes to shopping cart -->
-    <!-- if admin go to admin home -->
-     <!-- can add/edit/delete admins and users -->
-     <!-- can add/edit/delete items in the store -->
+<!-- Clicks the Login button -->
+<!-- if user go back to user home -->
+<!-- Goes to shopping -->
+<!-- Goes to shopping cart -->
+<!-- if admin go to admin home -->
+<!-- can add/edit/delete admins and users -->
+<!-- can add/edit/delete items in the store -->
 
 
-     <?php include("user_home.php"); ?>
 <?php include("user_home.php"); ?>
 
 
 <button onclick="showpage('user_home');">user_home</button>
 
 <script>
-function showpage(pagename) {
-  var page = document.getElementById(pagename);
-  page.setAttribute("style", "display: block");
-}
+  function showpage(pagename) {
+      var page = document.getElementById(pagename);
+      page.setAttribute("style", "display: block");
+  }
 </script>
 <!-- Can admins also login as regular users? -->
 <!-- Logins will be done with a modal -->
