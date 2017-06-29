57a6a1ad4e54e758a771bd94ad99699a94f8ca58
LydiaStreuer <L.streuer@hotmail.com>
2017-06-29 09:05:06 +0200

    f
    f

--- admin_inventory.php
+++ admin_inventory.php
@@ -35,7 +35,12 @@ $categorie=$_POST['categorie'];
 $afbeelding=$_POST['afbeelding'];
 $prijs=$_POST['prijs'];
 $sku=$_POST['sku'];
-mysql_query("INSERT INTO boeken VALUES('$id', '$prijs', '$afbeelding', '$boeknaam', '$categorie', '$sku')");
+// $sql("INSERT INTO boeken VALUES('1','3','j.jpg','t','auto','500')");
+// if(mysqli_query($con, $sql)){
+//     echo "Records added successfully.";
+// } else{
+//     echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+// }
 ?>
 
 <h1>Admin inventory edit page</h1>
@@ -54,42 +59,78 @@ mysql_query("INSERT INTO boeken VALUES('$id', '$prijs', '$afbeelding', '$boeknaa
 		</tr>
 	</thead>
 	<tbody>
+	<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+	  <div class="modal-content">
+	    <span class="close">&times;</span>
+	    <!-- <link rel="stylesheet" type="text/css" href=""> -->
+	    <div id="forminvoer">
+		    <form method="post">
+			    ID :
+			    </br>
+			    <input id="input" name="id" value="" type="text">
+			    </br>
+			    BoekNaam :
+			    </br>
+			    <input id="input" name="boeknaam" value="" type="text">
+			    </br>
+			    Categorie :
+			    </br>
+			    <select>
+					<option value="categorie">Auto</option>
+					<option value="categorie">Brommer</option>
+					<option value="categorie">Bus</option>
+					<option value="categorie">Motor</option>
+					<option value="categorie">Spiegels</option>
+					<option value="categorie">Vrachtwagen</option>
+				</select>
+			    </br>
+			    Afbeelding :
+			    </br>
+			    <input id="input" name="afbeelding" value="" type="text">
+			    </br>
+			    Prijs :
+			    </br>
+			    <input id="input" name="prijs" value="" type="text">
+			    </br>
+			    Sku :
+			    </br>
+			    <input id="input" name="sku" value="" type="text">
+			    </br>
+			    </br>
+			    <button id="change2">Toevoegen</button>
+		    </form>
+	    </div>
+	  </div>
+	</div>
 		<script>
-		    function Add() {
-				my_window = window.open("", "mywindow1", "status=1,width=350,height=350");
-				// my_window.document.write('<link rel="stylesheet" type="text/css" href="">');
-				my_window.document.write('<div id="forminvoer">');
-				my_window.document.write('<form method="post">');
-				my_window.document.write('ID :');
-				my_window.document.write('</br>');
-				my_window.document.write('<input id="input" name="id" value="" type="text">');
-				my_window.document.write('</br>');
-				my_window.document.write('BoekNaam :');
-				my_window.document.write('</br>');
-				my_window.document.write('<input id="input" name="boeknaam" value="" type="text">');
-				my_window.document.write('</br>');
-				my_window.document.write('Categorie :');
-				my_window.document.write('</br>');
-				my_window.document.write('<input id="input" name="categorie" value="" type="text">');
-				my_window.document.write('</br>');
-				my_window.document.write('Afbeelding :');
-				my_window.document.write('</br>');
-				my_window.document.write('<input id="input" name="afbeelding" value="" type="text">');
-				my_window.document.write('</br>');
-				my_window.document.write('Prijs :');
-				my_window.document.write('</br>');
-				my_window.document.write('<input id="input" name="prijs" value="" type="text">');
-				my_window.document.write('</br>');
-				my_window.document.write('Sku:');
-				my_window.document.write('</br>');
-				my_window.document.write('<input id="input" name="sku" value="" type="text">');
-				my_window.document.write('</br>');	
-				my_window.document.write('</br>');	
-				my_window.document.write('<button id="change2">Toevoegen</button>');
-				my_window.document.write('</form>');
-				my_window.document.write('</div>');						
-			}
-		    </script>
+		// Get the modal
+		var modal = document.getElementById('myModal');
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("add");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		btn.onclick = function() {
+		    modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		    modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		}
+		</script>
 		<?php
 		// if (is_int($_GET["id"]) {
 		//     $query2 = "DELETE FROM boeken WHERE boekid =". $_GET["id"];
@@ -109,7 +150,7 @@ mysql_query("INSERT INTO boeken VALUES('$id', '$prijs', '$afbeelding', '$boeknaa
 		    	printf("<td>". $row['1'] ."</td>");
 		    	printf("<td>". $row['5'] ."</td>");
 		    	printf("<td><button onclick='Change()' id='change'><i class='fa fa-pencil-square-o fa-2x' aria-hidden='true'> </i></button></td>");
-		    	printf("<td><a href='admin_index.php?id=" . $row['id']. "'><i class='fa fa-trash fa-2x' aria-hidden='true'></i></a></td>");
+		    	printf("<td><button onclick='Delete()' id='change'><i class='fa fa-trash fa-2x' aria-hidden='true'></i></button></td>");
 		    	printf("</tr>");
 		    }
 		  // Free result set
