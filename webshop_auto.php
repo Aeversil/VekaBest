7c052d2586e0cad1fbb039e5bda3bf9cbee9a01a
Steffan <walliesteffan@hotmail.com>
2017-07-03 13:31:45 +0200

    action form inlaten vullen met winkelwagen pagina

--- webshop_auto.php
+++ webshop_auto.php
@@ -16,7 +16,8 @@
           <?php
           echo "<img src=" . $row["boekafbeelding"] . "></img><span>Productnummer: " . $row["boeksku"] . "</span><span>Boek: " . $row["boeknaam"] . "</span><span>Prijs: €" . $row["boekprijs"] . "</span>";
           ?>
-        <form action="VekaBest.php" method="$_POST">
+        <!-- ACTION MOET WORDEN INGEVULD MET LATERE PHP PAGINA -->
+        <form action=".php" method="$_POST">
           Aantal:<input type="text" name="aantal">
           <input type="image" src="stockvekafotos/winkelmand.png" width="25px" height="25px" border="0" alt="Submit" value="aantal"/>
         </form>
