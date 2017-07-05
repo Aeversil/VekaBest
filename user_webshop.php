50ef1efe1a13c3eb843252c51c925250477fa369
Steffan <walliesteffan@hotmail.com>
2017-07-05 09:45:03 +0200

    Webshop images aangepast

--- user_webshop.php
+++ user_webshop.php
@@ -1,28 +1,29 @@
 <div id="user_webshop" style="display: none">
-  <div class="col-xs-8 col-md-offset-2 panel-color" >
-    <div onclick="showPage('webshop_auto')" class="col-md-4 panel-body categories button-color-darkblue">
+  <!-- <div class="webshopbutton"> -->
+  <!-- <div class="col-xs-8 col-md-offset-2 panel-color"> -->
+    <div onclick="showPage('webshop_auto')" class="panel-body categories button-color-darkblue">
       <!-- Auto -->
-      <img class="webshopfoto" value="Auto" src="stockvekafotos/theorieboek_B.jpg"/>
+      <img class="webshopfoto" src="stockvekafotos/theorieboek_B.jpg"/>
     </div>
-    <div onclick="showPage('webshop_brommer')" class="col-md-4 panel-body categories button-color-yellow">
+    <div onclick="showPage('webshop_brommer')" class="panel-body categories button-color-yellow">
       <!-- Brommer -->
-      <img class="webshopfoto" value="Auto" src="stockvekafotos/Brommer.jpg"/>
+      <img class="webshopfoto" src="stockvekafotos/Brommer.jpg"/>
     </div>
-    <div onclick="showPage('webshop_motor')" class="col-md-4 panel-body categories button-color-green">
+    <div onclick="showPage('webshop_motor')" class="panel-body categories button-color-green">
       <!-- Motor -->
-      <img class="webshopfoto" value="Auto" src="stockvekafotos/motor.jpg"/>
+      <img class="webshopfoto" src="stockvekafotos/motor.jpg"/>
     </div>
-    <div onclick="showPage('webshop_vrachtwagen')" class="col-md-4 panel-body categories button-color-orange">
+    <div onclick="showPage('webshop_vrachtwagen')" class="panel-body categories button-color-orange">
       <!-- Vrachtwagen -->
-      <img class="webshopfoto" value="Auto" src="stockvekafotos/vrachtwagen.jpg"/>
+      <img class="webshopfoto" src="stockvekafotos/vrachtwagen.jpg"/>
     </div>
-    <div onclick="showPage('webshop_bus')" class="col-md-4 panel-body categories button-color-orange">
+    <div onclick="showPage('webshop_bus')" class=" panel-body categories button-color-orange">
       <!-- Bus -->
-      <img class="webshopfoto" value="Auto" src="stockvekafotos/vrachtwagen.jpg"/>
+      <img class="webshopfoto" src="stockvekafotos/vrachtwagen.jpg"/>
     </div>
-    <div onclick="showPage('webshop_spiegel')" class="col-md-4 panel-body categories button-color-blue">
+    <div onclick="showPage('webshop_spiegel')" class="panel-body categories button-color-blue">
       <!-- Spiegel -->
-      <img class="webshopfoto" value="Auto" src="stockvekafotos/Lesspiegels-Rijschool-Verdoorn.jpg"/>
+      <img class="webshopfoto" src="stockvekafotos/Lesspiegels-Rijschool-Verdoorn.jpg"/>
     </div>
-  </div>
+  <!-- </div> -->
 </div>
